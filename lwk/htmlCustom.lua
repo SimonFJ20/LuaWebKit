058355615675569b10c7470d82fab643fda1c6ef
SimonFJ20 <simonfromjakobsen@gmail.com>
2021-06-16 20:23:13 +0200

    Create htmlCustom.lua

---
+++ lwk/htmlCustom.lua
@@ -0,0 +1,34 @@
+
+local lwkUtils = require('lwk.lwkUtils');
+
+local module = {};
+
+local function renderHtml(self)
+    if (self.children) then
+        local innerHtml = lwkUtils:renderInnerHtml(self);
+        local html =  '<%s>%s</%s>';
+        html = html:format(self.tagname, innerHtml, self.tagname);
+        return html;
+    else
+        local html =  '<%s/>';
+        html = html:format(self.tagname);
+        return html;
+    end
+end
+
+function module:init_HtmlCustom(tagname, isParent)
+    local htmlCustom = {
+        instanceof = {
+            'HtmlCustomElement',
+            'HtmlElement'
+        },
+        renderHtml = renderHtml,
+        tagname = tagname
+    };
+    if (isParent) then
+        htmlCustom.children = {};
+    end
+    return htmlCustom;
+end
+
+return module;
